9d3d1a0e506676fe59bece5d6ac1f1538b0dbccb
MarMis <30797189+MarmisDev@users.noreply.github.com>
2019-01-30 14:45:49 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1548834284,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/convert-sheet0.png",
+		"images/buy-sheet0.png",
+		"images/audiostreams.png",
+		"images/mergedstreams.png",
+		"images/mp3.png",
+		"images/videostreams.png",
+		"images/videos.png",
+		"images/newname.png",
+		"images/downloadnow.png",
+		"images/logo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pacifico-regular-webfont.woff",
+		"stylesheet.css"
+	]
+}
